d3a0b83bdc6b2e634e4880cd5f57dd172cdcc49a
yingfeng <383373755@qq.com>
2020-08-07 14:09:50 +0800

    git init: 主要存放学习代码，包含javase源码学习以及算法学习代码

---
+++ ContainerClassDemo/src/ArrayListDemo.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+
+    public static void main(String[] args) {
+        List<Object> demo = new ArrayList<>();
+
+
+    }
+}
