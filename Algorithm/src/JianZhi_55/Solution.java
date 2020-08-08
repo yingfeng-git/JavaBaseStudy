28b43eedb4f743b371fd396536a8d08ab9785042
yingfeng <383373755@qq.com>
2020-08-08 13:34:50 +0800

    剑指第一题

---
+++ Algorithm/src/JianZhi_55/Solution.java
@@ -0,0 +1,36 @@
+package JianZhi_55;
+
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    //对二叉树还不熟，太难了
+    public boolean isBalence(TreeNode tree){
+        Stack<TreeNode> treeStack = new Stack<>();
+        return false;
+    }
+
+    public TreeNode createTree(Integer[] data){
+        Stack<TreeNode> treeNodes = new Stack<>();
+        TreeNode h = new TreeNode(data[0]);
+        treeNodes.push(h);
+        for(int i = 1; i < data.length-1; i++){
+            ;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
