272de498459f217b971d360718113784ac59720d
yingfeng <512365930@qq.com>
2020-09-23 15:58:22 +0800

    1.日常刷题 2.自定义ClassLoader 3.自定义ClassLoader破坏双亲委派机制，可以实现热加载

---
+++ JVM/src/pojo/Cat.java
@@ -0,0 +1,10 @@
+package pojo;
+
+public class Cat {
+    static  {
+        System.out.println("cat is loading...");
+    }
+    public void run(){
+        System.out.println("cat is running...");
+    }
+}
